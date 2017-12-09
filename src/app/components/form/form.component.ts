8d0f445c56a00e4d05f8c166bffdc99d7cde2c03
irvinalfaro11 <irvinalfaro11@gmail.com>
2017-12-10 03:00:55 +0800

    add form validators

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { NgClass, NgFor, NgIf } from '@angular/common';
+import { FormBuilder, FormGroup, Validators, FormControl, AbstractControl } from '@angular/forms';
+
 
 
 @Component({
@@ -7,16 +9,22 @@ import { NgClass, NgFor, NgIf } from '@angular/common';
   templateUrl: './form.component.html'
 })
 export class FormComponent implements OnInit {
-  // tableTitle = "Form Title";
-  constructor() { }
 
-  @Input() formTitle: String;
+  @Input() formLabel: String;
   @Input() numberOfInputs: Array<object>; //Number of input
   @Input() inputLabel: Array<string>; //Label of input
   @Input() inputClass: Array<string>; //Classname of input
   @Input() inputType: Array<string>;  //Type of input
 
+  private form: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
+
   ngOnInit() {
+    this.form = this.formBuilder.group({
+      '*': [null, Validators.required]
+    });
   }
 
 }
