7e5188a7ebcc88e6b762336bd26833cee7c36c51
irvinalfaro11 <irvinalfaro11@gmail.com>
2017-12-30 10:43:33 +0800

    applied change request

--- src/app/class/add-class/add-class.component.ts
+++ src/app/class/add-class/add-class.component.ts
@@ -13,8 +13,45 @@ import { MyClassActionCreator } from '../../store/action-creators/my-class.actio
 export class AddClassComponent implements OnInit {
   @select(s => s.courses.courses) courses;
   @select(s => s.instructors.instructors) instructors;
+  @select(s => s.session.program) program;
+  private programId: number;
   private myClassForm: FormGroup;
-  private programId: number = 5;
+  private academicYearData = [
+    {
+      value: '2017-2018',
+      name: '2017-2018'
+    }, {
+      value: '2018-2019',
+      name: '2018-2019'
+    }, {
+      value: '2019-2020',
+      name: '2019-2020'
+    }
+  ];
+  private termData = [
+    {
+      value: 1,
+      name: 'Term 1'
+    }, {
+      value: 2,
+      name: 'Term 2'
+    }, {
+      value: 3,
+      name: 'Term 3'
+    }
+  ];
+  private cycleData = [
+    {
+      value: 1,
+      name: 'Cycle 1'
+    }, {
+      value: 2,
+      name: 'Cycle 2'
+    }, {
+      value: 3,
+      name: 'Cycle 3'
+    }
+  ];
   constructor(
     private courseActionCreator: CourseActionCreator,
     private instructorActionCreator: InstructorActionCreator,
@@ -23,6 +60,11 @@ export class AddClassComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    this.program.subscribe(
+    (program => {
+      this.programId = program.id;
+    })
+    );
     this.courseActionCreator.GetCourse(this.programId);
     this.instructorActionCreator.GetInstructor(this.programId);
     this.myClassForm = this.formBuilder.group({
@@ -35,7 +77,6 @@ export class AddClassComponent implements OnInit {
   }
 
   submit() {
-    console.log(this.myClassForm.value);
     if(this.myClassForm.valid) {
       this.myClassActionCreator.CreateMyClass(this.programId, this.myClassForm.value);
     }
