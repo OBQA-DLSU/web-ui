c964bb9b88b790898dedbdcf902196dc73db3a8f
irvinalfaro11 <irvinalfaro11@gmail.com>
2017-12-30 13:32:07 +0800

    adjust styles and naming

--- src/app/class/add-class/add-class.component.ts
+++ src/app/class/add-class/add-class.component.ts
@@ -75,7 +75,7 @@ export class AddClassComponent implements OnInit {
       term: [null, Validators.required],
       academicYear: [null, Validators.required],
       cycle: [null, Validators.required],
-      CourseId: [null, Validators.required],
+      courseId: [null, Validators.required],
       instructorId: [this.instructorId, Validators.required]
     });
   }
