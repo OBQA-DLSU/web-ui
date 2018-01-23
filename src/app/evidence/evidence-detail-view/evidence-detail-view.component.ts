9a31ac3e740798e9c5e7693c6030eaded4b265e9
johnhigginsavila <johnhiggins.avila@gmail.com>
2018-01-24 01:58:20 +0800

    [#59]FEAT(UploadEvidence): Upload evidence function

--- src/app/evidence/evidence-detail-view/evidence-detail-view.component.ts
+++ src/app/evidence/evidence-detail-view/evidence-detail-view.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { IAssessment } from 'app/interfaces/assessment/assessment.interface';
+import { IMyClassView } from 'app/interfaces/myClass/my-class-view.interface';
 
 @Component({
   selector: 'app-evidence-detail-view',
@@ -9,10 +10,37 @@ import { IAssessment } from 'app/interfaces/assessment/assessment.interface';
 export class EvidenceDetailViewComponent implements OnInit {
 
   @Input() assessment: IAssessment;
-
+  @Input() myClass: IMyClassView;
   constructor() { }
 
-  ngOnInit() {
-  }
+  private evidenceMax: any;
+  private evidenceMin: any;
+  private evidenceMedian: any;
 
+  ngOnInit() {
+    this.evidenceMax = {
+      type: 'MAX',
+      assessmentId: this.assessment.id,
+      myClassId: this.myClass.id,
+      programSopiId: this.assessment.programSopiId,
+      programCourseId: this.myClass.programCourseId,
+      programId: this.myClass.programId
+    }
+    this.evidenceMedian = {
+      type: 'MEDIAN',
+      assessmentId: this.assessment.id,
+      myClassId: this.myClass.id,
+      programSopiId: this.assessment.programSopiId,
+      programCourseId: this.myClass.programCourseId,
+      programId: this.myClass.programId
+    }
+    this.evidenceMin = {
+      type: 'MIN',
+      assessmentId: this.assessment.id,
+      myClassId: this.myClass.id,
+      programSopiId: this.assessment.programSopiId,
+      programCourseId: this.myClass.programCourseId,
+      programId: this.myClass.programId
+    }
+  }
 }
